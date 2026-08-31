6184263a15babe7c4c4f7993a61b94ede2ddb5ce
kalkonkurt <kalkonkurt@gmail.com>
2026-08-31 11:43:14 +0200

    fix: added a11y configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,6 +14,24 @@ const eslintConfig = defineConfig([
     'out/**',
     'build/**',
     'next-env.d.ts',
+    {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    plugins: {
+      'jsx-a11y': eslintPluginJsxA11y,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      // ... any rules you want
+      'jsx-a11y/alt-text': 'error',
+    },
+    // ... others are omitted for brevity
+  },
   ]),
   prettier,
 ]);
