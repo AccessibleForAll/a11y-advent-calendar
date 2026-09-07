e32a939116392de4af134e60b246b8276992fdd1
David Lindström <david01lindstrom@gmail.com>
2026-09-07 09:44:29 +0200

    inputfield design completed

--- src/components/Inputfield/Inputfield.tsx
+++ src/components/Inputfield/Inputfield.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import styles from "./Inputfield.module.scss";
-
+import React from 'react';
+import styles from './Inputfield.module.scss';
 
 // props inputfield component accepts.
 
@@ -8,8 +7,10 @@ type InputFieldProps = {
   label: string;
   value?: string;
   placeholder?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-  type?: "text" | "textarea"; //text is single line input and textarea is multi line
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
+  type?: 'text' | 'textarea'; //text is single line input and textarea is multi line
   name?: string;
   rows?: number; //rows supports textarea height with number of rows.
   fullWidth?: boolean;
@@ -18,40 +19,40 @@ type InputFieldProps = {
 export default function InputField({
   label,
   value,
-  placeholder = "",
+  placeholder = '',
   onChange,
-  type = "text",
+  type = 'text',
   name,
   rows = 4,
   fullWidth = false,
 }: InputFieldProps) {
   return (
-    <div className={`${styles.wrapper} ${fullWidth ? styles.fullWidth : ""}`}>
-        <label className={styles.label} htmlFor ={name}>
-            {label}
-        </label>
+    <div className={`${styles.wrapper} ${fullWidth ? styles.fullWidth : ''}`}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
 
-        {type === "textarea" ? (
-            <textarea
-                id={name}
-                name={name}
-                className={styles.field}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                rows={rows}
-            />
-        ) : (
-            <input
-                id={name}
-                name={name}
-                className={styles.field}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                type="text"
-            />
-        )}
+      {type === 'textarea' ? (
+        <textarea
+          id={name}
+          name={name}
+          className={styles.field}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          rows={rows}
+        />
+      ) : (
+        <input
+          id={name}
+          name={name}
+          className={styles.field}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          type="text"
+        />
+      )}
     </div>
-    );
+  );
 }
