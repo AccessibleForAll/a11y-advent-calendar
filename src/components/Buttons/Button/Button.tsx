b47d45900ee4a9d2b8e31d5b661bf97d3f64e8fe
Anna Ek <anna.ek2@outlook.com>
2026-09-09 12:00:46 +0200

    refactor: simplify Button component by consolidating tone to variant and updating styles. Added type property

--- src/components/Buttons/Button/Button.tsx
+++ src/components/Buttons/Button/Button.tsx
@@ -2,60 +2,37 @@ import style from './Button.module.scss';
 import { type LucideIcon } from 'lucide-react';
 import { type ReactNode, type ButtonHTMLAttributes } from 'react';
 
-type ButtonTone =
-  'primary' | 'brand' | 'danger' | 'neutral' | 'signIn' | 'cancel';
+type ButtonVariant = 'primary' | 'secondary';
 
 export type ButtonProps = {
-  tone: ButtonTone;
+  variant: ButtonVariant;
   icon?: LucideIcon;
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const toneClasses: Record<ButtonTone, string[]> = {
+const variantClasses: Record<ButtonVariant, string[]> = {
   primary: [
-    style.outlined,
-    style['bg-off-white'],
-    style['border-black'],
+    style['bg-primary'],
+    style['border-primary'],
     style['text-primary'],
   ],
-  brand: [
-    style.filled,
+  secondary: [
     style['bg-secondary'],
+    style['border-match-secondary'],
     style['text-white'],
     style['font-bold'],
-  ],
-  danger: [
-    style.outlined,
-    style['bg-primary'],
-    style['text-primary'],
-    style['border-black'],
-  ],
-  neutral: [style.filled, style['bg-primary'], style['text-primary']],
-  signIn: [
-    style.outlined,
-    style['bg-off-white'],
-    style['border-primary'],
-    style['text-black'],
-    style['font-bold'],
-    style.sharper,
-  ],
-  cancel: [
-    style.outlined,
-    style['bg-off-white'],
-    style['border-primary'],
-    style['text-black'],
   ],
 };
 
 export default function Button({
-  tone,
+  variant,
   icon: Icon,
   children,
   ...rest
 }: ButtonProps) {
-  const className = [style.button, ...toneClasses[tone]].join(' ');
+  const className = [style.button, ...variantClasses[variant]].join(' ');
   return (
-    <button className={className} {...rest}>
+    <button type="button" className={className} {...rest}>
       {Icon && <Icon className={style.icon} size={'1rem'} aria-hidden="true" />}
       {children}
     </button>
