c498b045cc4d8d6c8957dc61ba838baafcaabe0b
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-09-10 13:41:10 +0200

    57-feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import FooterStyles from './Footer.module.scss'
+export default function Footer() {
+  return (
+    <footer className={FooterStyles.area}>
+            <p>© 2026 AxessLab</p>
+            <p>All <strong>human</strong> rights preserved.</p>
+    </footer>
+  )
+}
