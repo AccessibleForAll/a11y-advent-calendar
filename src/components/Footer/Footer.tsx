dc2e80e95c258776f8ae60650088053fe47c4f0e
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-09-10 19:51:12 +0200

    fix: address footer review feedback

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import FooterStyles from './Footer.module.scss'
 export default function Footer() {
   return (
-    <footer className={FooterStyles.area}>
+    <footer className={FooterStyles.footerContainer}>
             <p>© 2026 AxessLab</p>
             <p>All <strong>human</strong> rights preserved.</p>
     </footer>
