cea6ff849dcc50bb00f7d56af35834e3829f6af7
David Lindström <david01lindstrom@gmail.com>
2026-09-11 14:54:05 +0200

    Added daybuttons in grid and adjusted to correct scss for the whole calendar page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
-import Image from 'next/image';
-import styles from './page.module.scss';
+'use client';
 
+import styles from './page.module.scss';
+import DayButton from '@/components/DayButton/DayButton';
+
+const totalDays = 24;
 
 export default function Home() {
 
-  return (
+    const days = Array.from({ length: totalDays }, (_, i) => i + 1);
 
+
+  return (
     <main className={styles.main}>
       <h1 className={styles.title}>Accessibility Advent Calendar</h1>
 
       <div className={styles.intro}>
-        <p>24 bite-sized web accessibility tips, one per door.
-        <br />Click any unlocked door to discover an accessibility tip.
-        <br />Locked days are still to come.</p>
+        <p>24 bite-sized web accessibility tips, one per door.</p>
+        <p>Click any unlocked door to discover an accessibility tip.</p>
+        <p>Locked days are still to come.</p>
+      </div>
+
+        <div className={styles.grid}>
+          {days.map((day) => (
+            <DayButton
+                key={day}
+                day={day.toString()}
+                isLocked={false}
+                onClick={() => console.log(`Clicked day ${day}`)}
+            />
+          ))}
       </div>
     </main>
-  );
+    );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import FooterStyles from './Footer.module.scss'
+import FooterStyles from './Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={FooterStyles.footerContainer}>
-            <p>© 2026 AxessLab</p>
-            <p>All <strong>human</strong> rights preserved.</p>
+      <p>© 2026 AxessLab</p>
+      <p>
+        All <strong>human</strong> rights preserved.
+      </p>
     </footer>
-  )
+  );
 }
