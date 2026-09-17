5aef678c6d411ee9ca8d5e867f621830cefcf513
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-09-17 19:08:20 +0200

    feat: build responsive admin login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,40 @@
+'use client';
+import Link from 'next/link';
+import Card from '@/components/Card/Card';
+import InputField from '@/components/Inputfield/Inputfield';
+import Button from '@/components/Buttons/Button/Button';
+import basicStyles from '@/app/page.module.scss';
+import textStyles from '@/components/Inputfield/Inputfield.module.scss';
+import loginStyles from '@/app/login/login.module.scss';
+import { MoveLeft } from 'lucide-react';
+
 export default function loginPage() {
-  return <h1>Admin Panel Login</h1>;
+  function handleClick() {
+    alert('Clicked');
+  }
+  return (
+    <>
+      <div className={loginStyles.upper}>
+        <h1 className={basicStyles.title}>Admin Panel Login</h1>
+        <p className={textStyles.label}>Accessibility Advent Calendar</p>
+      </div>
+      <div className={loginStyles.container}>
+        <Card className={loginStyles.loginCard}>
+          <InputField name="userName" label="Username" />
+          <InputField name="password" label="Password" />
+          <div className={loginStyles.buttonWrapper}>
+            <Button variant="primary" onClick={handleClick}>
+              Sign in
+            </Button>
+          </div>
+        </Card>
+      </div>
+      <div className={loginStyles.back}>
+        <Link href="/">
+          <MoveLeft />
+          Back to calendar
+        </Link>
+      </div>
+    </>
+  );
 }
