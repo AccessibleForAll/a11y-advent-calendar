b5fb7637f55c89fcabf912af12744cfd2ca8b1b9
kalkonkurt <kalkonkurt@gmail.com>
2026-09-17 15:37:57 +0200

    changing computer

--- src/app/admin/manageDays/page.tsx
+++ src/app/admin/manageDays/page.tsx
@@ -1,22 +1,52 @@
 'use client';
+import { useState } from 'react';
+import { Trash2Icon, PencilLine } from 'lucide-react';
+
+import { days } from '../../../../data/days';
+import pageStyles from '@/app/page.module.scss';
+
 import Card from '@/components/Card/Card';
 import cardStyles from '@/components/Card/Card.module.scss';
-import { days } from '../../../../data/days';
 import Button from '@/components/Buttons/Button/Button';
-import { Trash2Icon, PencilLine } from 'lucide-react';
-export default function manageDaysPage() {
+import Modal from '@/components/Modal/Modal';
+import modalStyles from '@/components/Modal/Modal.module.scss';
+
+import manageDaysStyles from './manageDays.module.scss';
+
+export default function ManageDaysPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
-      <h1>Manage Days</h1>
+      <div className={manageDaysStyles.manageDaysHeader}>
+        <h1
+          className={`${pageStyles.title} ${manageDaysStyles.manageDaysTitle}`}
+        >
+          Manage Days
+        </h1>
+        <Button variant="secondary" onClick={() => setIsModalOpen(true)}>
+          + New Day
+        </Button>
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Create Day"
+      >
+        <p className={modalStyles.modalText}>
+          This is a modal for creating a new day. You can add the form or
+          content here.
+        </p>
+      </Modal>
       <ul className={cardStyles.cardGrid}>
         {days.slice(0, 4).map((d) => (
           <li key={d.day}>
             <Card>
-              <div className={cardStyles.manageDaysWrapper}>
-                <p className={cardStyles.dayLabel}>Day {d.day}</p>
+              <div className={manageDaysStyles.manageDaysWrapper}>
+                <p className={manageDaysStyles.dayLabel}>Day {d.day}</p>
                 <h2>{d.title}</h2>
-                <p className={cardStyles.clampedText}>{d.text}</p>
-                <div className={cardStyles.manageDaysButton}>
+                <p className={manageDaysStyles.clampedText}>{d.text}</p>
+                <div className={manageDaysStyles.manageDaysButton}>
                   <Button
                     variant="primary"
                     icon={PencilLine}
