9c6d55193a9e7eaed2beca3b4133f71764f29d82
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-09-20 19:18:34 +0200

    fix: address review comments on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link';
 import Card from '@/components/Card/Card';
 import InputField from '@/components/Inputfield/Inputfield';
 import Button from '@/components/Buttons/Button/Button';
-import basicStyles from '@/app/page.module.scss';
-import textStyles from '@/components/Inputfield/Inputfield.module.scss';
 import loginStyles from '@/app/login/login.module.scss';
 import { MoveLeft } from 'lucide-react';
 
@@ -14,22 +12,26 @@ export default function loginPage() {
   }
   return (
     <>
-      <div className={loginStyles.upper}>
-        <h1 className={basicStyles.title}>Admin Panel Login</h1>
-        <p className={textStyles.label}>Accessibility Advent Calendar</p>
+      <div className={loginStyles.loginHeader}>
+        <h1>Admin Panel Login</h1>
+        <p className={loginStyles.subtitle}>Accessibility Advent Calendar</p>
       </div>
-      <div className={loginStyles.container}>
-        <Card className={loginStyles.loginCard}>
-          <InputField name="userName" label="Username" />
-          <InputField name="password" label="Password" />
-          <div className={loginStyles.buttonWrapper}>
-            <Button variant="primary" onClick={handleClick}>
-              Sign in
-            </Button>
-          </div>
-        </Card>
+      <div className={loginStyles.loginContainer}>
+        <div className={loginStyles.loginCard}>
+          <Card>
+            <div className={loginStyles.loginContent}>
+              <InputField name="userName" label="Username" />
+              <InputField name="password" label="Password" />
+              <div className={loginStyles.buttonWrapper}>
+                <Button variant="primary" onClick={handleClick}>
+                  Sign in
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </div>
       </div>
-      <div className={loginStyles.back}>
+      <div className={loginStyles.backToCalendar}>
         <Link href="/">
           <MoveLeft />
           Back to calendar
