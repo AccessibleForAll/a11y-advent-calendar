8e1bf8fe5ee3cf49398b2f1f43afab122bc480da
Anna Ek <anna.ek2@outlook.com>
2026-09-22 16:04:11 +0200

    feat: implement form fields and buttons in Create Day modal

--- src/app/admin/manageDays/page.tsx
+++ src/app/admin/manageDays/page.tsx
@@ -3,13 +3,14 @@ import { useState } from 'react';
 import { Trash2Icon, PencilLine } from 'lucide-react';
 
 import { days } from '../../../../data/days';
-import pageStyles from '@/app/page.module.scss';
 
 import Card from '@/components/Card/Card';
 import Button from '@/components/Buttons/Button/Button';
 import Modal from '@/components/Modal/Modal';
-import modalStyles from '@/components/Modal/Modal.module.scss';
+import InputField from '@/components/Inputfield/Inputfield';
 
+import pageStyles from '@/app/page.module.scss';
+import modalStyles from '@/components/Modal/Modal.module.scss';
 import manageDaysStyles from './ManageDays.module.scss';
 
 export default function ManageDaysPage() {
@@ -32,10 +33,17 @@ export default function ManageDaysPage() {
         onClose={() => setIsModalOpen(false)}
         title="Create Day"
       >
-        <p className={modalStyles.modalText}>
-          This is a modal for creating a new day. You can add the form or
-          content here.
-        </p>
+        <InputField name="date" label="Date:" type="text"></InputField>
+        <InputField name="heading" label="Heading:" type="text"></InputField>
+        <InputField name="text" label="Text:" type="textarea"></InputField>
+        <InputField name="linkText" label="Link Text:" type="text"></InputField>
+        <InputField name="linkUrl" label="Link URL:" type="text"></InputField>
+        <Button variant="primary" onClick={() => console.log('Cancel pressed')}>
+          Cancel
+        </Button>
+        <Button variant="secondary" onClick={() => console.log('Save pressed')}>
+          Save
+        </Button>
       </Modal>
       <ul className={manageDaysStyles.cardGrid}>
         {days.slice(0, 4).map((d) => (
